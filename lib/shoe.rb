149b708d5627845e9cda95a8e7528001939709c9
Breanna <breanna@breannawebster.com>
2020-11-18 12:15:13 -0500

    commit tree 68b0bb24eb0b690ca6e1e15ea9e5230103667153

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
-# Make your shoe class here!
+class Shoe 
+    attr_accessor :color , :size , :material , :condition
+    attr_reader :cobble
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+
+       def cobble
+         self.condition = "new"
+
+         puts "Your shoe is as good as new!"
+
+       end
+
+       
+end
